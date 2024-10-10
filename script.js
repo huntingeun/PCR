089703bfc16e329ccd3b810e421fbc83cf814cde
huntingeun <65370206+huntingeun@users.noreply.github.com>
2024-10-10 13:17:25 +0900

    -

--- script.js
+++ script.js
@@ -75,15 +75,3 @@ async function validateCode(inputCode) {
     }
 }
 
-// JavaScript to dynamically set --vh to the viewport height
-  function setViewportHeight() {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-// Set the height on page load
-setViewportHeight();
-
-// Update the height on resize
-window.addEventListener('resize', setViewportHeight);
-
